ff5ae8e3ed4f16754f9645fb44e883606bbdf80d
Justin Liu <liujustin53@gmail.com>
2025-03-24 23:44:53 -0400

    bullet spawning

--- Assets/Scripts/Justin/CSVReader.cs
+++ Assets/Scripts/Justin/CSVReader.cs
@@ -7,9 +7,8 @@ using Unity.VisualScripting;
 [Inspectable]
 public struct BulletSpawnData
 {
-    public int x;
-    public int y;
-    public int direction; // 1 = up, 2 = right, 3 = down, 4 = left
+    public Vector2 position;
+    public int direction; // 0 = up, 1 = left, 2 = down, 3 = right
     public int turn; // the turn the bullet spawns on
 
     public BulletSpawnData(string[] values)
@@ -28,8 +27,7 @@ public struct BulletSpawnData
                 );
             }
         }
-        x = intValues[0];
-        y = intValues[1];
+        position = new Vector2(intValues[0], intValues[1]);
         direction = intValues[2];
         turn = intValues[3];
     }
