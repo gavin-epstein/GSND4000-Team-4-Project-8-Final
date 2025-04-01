558985439bcc22b2b9373076e0fdab7d9da853ee
Justin Liu <liujustin53@gmail.com>
2025-04-01 03:31:41 -0400

    finish(?) algo

--- Assets/Scripts/Justin/BulletSpawnAlgo.cs
+++ Assets/Scripts/Justin/BulletSpawnAlgo.cs
@@ -3,6 +3,13 @@ using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 
+[Inspectable]
+public struct Bullet
+{
+    public Vector2 position; // the position of the bullet on the board
+    public int direction; // up = 0, left = 1, down = 2, right = 3
+}
+
 public class BulletSpawner : Unit
 {
     enum BoardState
@@ -14,12 +21,6 @@ public class BulletSpawner : Unit
         BulletDown = 1 << 3,
         BulletRight = 1 << 4,
         Bullet = BulletUp | BulletLeft | BulletDown | BulletRight,
-    }
-
-    struct Bullet
-    {
-        public Vector2 position;
-        public int direction; // follows same convention as BoardState
     }
 
     class BoardNode
@@ -75,10 +76,10 @@ public class BulletSpawner : Unit
     // If there are not enough valid spawns, it will return as many as possible.
     private List<Bullet> GetBulletSpawns(int[,] board, int numBulletsToSpawn)
     {
-        int boardSize = board.GetLength(0);
         // First, create a directed graph where each node is a tile and each layer is a turn.
         // A connection between two nodes is a valid move for the player that will not hit a bullet.
         // The array is indexed by (turn, row, col) or (turn, y, x)
+        int boardSize = board.GetLength(0);
         BoardNode[,,] boardGraph = new BoardNode[boardSize, boardSize, boardSize];
         for (int i = 0; i < boardSize; i++)
         {
@@ -175,24 +176,103 @@ public class BulletSpawner : Unit
             }
         }
 
-        List<List<Bullet>> validSpawnSets = new List<List<Bullet>>();
+        // List of list of indices of valid spawn locations
+        // Each list of indices represents a set of bullets that can be spawned
+        List<List<int>> validSpawnSets = new List<List<int>>();
+        for (int i = 0; i < spawnLocations.Count; i++)
+        {
+            // Check if the spawn location is valid
+            if (IsValidSpawn(boardGraph, spawnLocations[i]))
+            {
+                // If it is valid, add it to the list of valid spawn locations
+                List<int> spawnSet = new List<int>();
+                spawnSet.Add(i);
+                validSpawnSets.Add(spawnSet);
+            }
+        }
 
-        return new List<Bullet>();
+        // Add more bullets until you can't add any more
+        while (validSpawnSets[0].Count < numBulletsToSpawn)
+        {
+            List<List<int>> newValidSpawnSets = new List<List<int>>();
+            for (int i = 0; i < validSpawnSets.Count; i++)
+            {
+                List<int> spawnSet = validSpawnSets[i];
+                // look forward from the index of the last bullet in the spawn set
+                // to avoid duplicates
+                for (int j = spawnSet[spawnSet.Count - 1] + 1; j < spawnLocations.Count; j++)
+                {
+                    // Check if the spawn location is valid
+                    if (IsValidSpawn(boardGraph, spawnLocations[j]))
+                    {
+                        // If it is valid, add it to the list of valid spawn locations
+                        List<int> newSpawnSet = new List<int>(spawnSet);
+                        newSpawnSet.Add(j);
+                        newValidSpawnSets.Add(newSpawnSet);
+                    }
+                }
+            }
+            if (newValidSpawnSets.Count == 0)
+            {
+                break; // No more valid spawn sets, break out of the loop
+            }
+            validSpawnSets = newValidSpawnSets; // Update the valid spawn sets
+        }
+
+        // Convert the valid spawn sets to bullet objects
+        List<Bullet> bullets = new List<Bullet>();
+        for (int i = 0; i < validSpawnSets[0].Count; i++)
+        {
+            int spawnIdx = validSpawnSets[0][i];
+            Vector2 spawnPos = spawnLocations[spawnIdx];
+            Vector2 posIdx = IndexToPosition(spawnPos, boardSize);
+            int direction = 0;
+            // up = 0, left = 1, down = 2, right = 3
+            if (spawnPos.y == boardSize - 1)
+            {
+                direction = 0; // up
+            }
+            else if (spawnPos.x == boardSize - 1)
+            {
+                direction = 1; // left
+            }
+            else if (spawnPos.y == 0)
+            {
+                direction = 2; // down
+            }
+            else if (spawnPos.x == 0)
+            {
+                direction = 3; // right
+            }
+            Bullet bullet = new Bullet { position = posIdx, direction = direction, };
+            bullets.Add(bullet);
+        }
+        return bullets;
     }
 
     // Converts a 2D array index to a 2D position on the board
-    // Multidimensional arrays are stored in row-major order
-    // and (0,0) is the top left of the array,
-    // while (0,0) is the center of the board.
+    // (0,0) is the top left of the array,
+    // (0,0) is the center of the board.
     /* Example: size = 3
-    *   (0,0) (0,1) (0,2)    (-1,1)  (0,1)  (1,1)
-    *   (1,0) (1,1) (1,2) -> (-1,0)  (0,0)  (1,0)
-    *   (2,0) (2,1) (2,2)    (-1,-1) (0,-1) (1,-1)
+    *   (0,0) (1,0) (2,0)    (-1,1)  (0,1)  (1,1)
+    *   (0,1) (1,1) (2,1) -> (-1,0)  (0,0)  (1,0)
+    *   (0,2) (1,2) (2,2)    (-1,-1) (0,-1) (1,-1)
     */
-    private Vector2 IndexToPosition(int row, int col, int size)
+    private Vector2 IndexToPosition(Vector2 idx, int size)
     {
         int center = size / 2;
-        return new Vector2(col - center, center - row);
+        return new Vector2(idx.x - center, center - idx.y);
+    }
+
+    /* Example: size = 3
+    *   (-1,1)  (0,1)  (1,1)     (0,0) (1,0) (2,0)
+    *   (-1,0)  (0,0)  (1,0)  -> (0,1) (1,1) (2,1)
+    *   (-1,-1) (0,-1) (1,-1)    (0,2) (1,2) (2,2)
+    */
+    private Vector2 PositionToIndex(Vector2 pos, int size)
+    {
+        int center = size / 2;
+        return new Vector2(pos.x + center, center - pos.y);
     }
 
     // Advances the board by one turn, moving the bullets
@@ -233,13 +313,83 @@ public class BulletSpawner : Unit
         return nextBoard;
     }
 
-    private bool IsValidSpawn(int[,] board, int row, int col)
+    // Checks if spawning a bullet at the given location will completely cut off
+    // all paths for the player. This is done by removing node connections at each
+    // turn the bullet will be on.
+    private bool IsValidSpawn(BoardNode[,,] boardGraph, Vector2 posIdx)
     {
-        // Check if the tile is on the edge of the board
-        if (row == 0 || row == board.GetLength(0) - 1 || col == 0 || col == board.GetLength(1) - 1)
+        // determine the direction of the bullet
+        Vector2 direction = new Vector2(0, 0);
+        int width = boardGraph.GetLength(2);
+        int height = boardGraph.GetLength(1);
+        if (posIdx.x == 0)
         {
-            return true;
+            direction = new Vector2(1, 0); // right
         }
-        return false;
+        else if (posIdx.x == width - 1)
+        {
+            direction = new Vector2(-1, 0); // left
+        }
+        else if (posIdx.y == 0)
+        {
+            direction = new Vector2(0, 1); // down
+        }
+        else if (posIdx.y == height - 1)
+        {
+            direction = new Vector2(0, -1); // up
+        }
+
+        Queue<BoardNode> queue = new Queue<BoardNode>();
+
+        // find all the nodes that the bullet will be on
+        for (int turn = 0; turn < boardGraph.GetLength(0); turn++)
+        {
+            Vector2 currentPos = posIdx + direction * turn;
+            int row = (int)currentPos.y;
+            int col = (int)currentPos.x;
+            BoardNode currentNode = boardGraph[turn, row, col];
+            // check if the node is reachable, don't add it to the queue if it is not
+            // to avoid extra work
+            if (currentNode.prev.Count != 0)
+            {
+                queue.Enqueue(currentNode);
+                // remove all connections to the previous turn
+                foreach (BoardNode prevNode in currentNode.prev)
+                {
+                    prevNode.next.Remove(currentNode);
+                }
+                currentNode.prev.Clear();
+            }
+        }
+
+        // Remove all forward connections from the nodes
+        while (queue.Count > 0)
+        {
+            BoardNode currentNode = queue.Dequeue();
+            foreach (BoardNode nextNode in currentNode.next)
+            {
+                nextNode.prev.Remove(currentNode);
+                if (nextNode.prev.Count == 0)
+                {
+                    queue.Enqueue(nextNode); // add to queue if it has no more connections
+                }
+            }
+            currentNode.next.Clear();
+        }
+
+        // Check if there are any connections to the last turn
+        int lastTurn = boardGraph.GetLength(0) - 1;
+        for (int row = 0; row < boardGraph.GetLength(1); row++)
+        {
+            for (int col = 0; col < boardGraph.GetLength(2); col++)
+            {
+                BoardNode currentNode = boardGraph[lastTurn, row, col];
+                if (currentNode.prev.Count > 0)
+                {
+                    return true; // valid spawn
+                }
+            }
+        }
+        return false; // invalid spawn, all paths are blocked
     }
 }
